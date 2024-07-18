5eb80b073bc03b478e17973bbe5770f9a011cd28
Imrane Bouhaddou <Imranebhd8gmail.com>
2024-07-18 12:48:57 +0100

    Ex 1

---
+++ javascript/javascript.js
@@ -0,0 +1,9 @@
+var UserName = prompt("enteyourname");
+var Age = prompt("enteryourage");
+var x = prompt("enter a number")
+var y = prompt("enter an umber ")
+console.log("x=", x)
+console.log("Y=", y)
+console.log("hello "+UserName+" your age is "+Age)
+if (Age < 10 )
+console.log("you are samall")
