48f4e687ddce47345a0a484a16fe3c9ed0774e63
Imrane Bouhaddou <Imranebhd8gmail.com>
2024-07-22 06:52:16 +0100

    Part3

--- javascript/EX1.js
+++ javascript/EX1.js
@@ -5,3 +5,33 @@ if (Age < 18){
  if (Age > 18) {
     alert("you are an adult")
  }
+
+    //Part 2: if...else Condition
+    /*Create a program that asks the user to enter their favorite color using prompt.
+Use an if...else condition to check if the favorite color is "blue".
+If the color is "blue", log a message saying "Blue is a cool color!"
+Otherwise, log a message saying "That's a nice color too!"*/
+
+var color = prompt("your favorit color");
+if (Color = "blue") {
+   console.log ("Blue is a cool color!");
+}
+else if {console.log ("That's a nice color too!")};
+//Part 3: if...else if...else Condition
+/*Create a program that asks the user to enter a number between 1 and 10 using prompt.
+Use an if...else if...else condition to check the number:
+If the number is 1, log "You entered one."
+If the number is 2, log "You entered two."
+If the number is 3, log "You entered three."
+Otherwise, log "Number is not between 1 and 3."*/
+var number = prompt("pick a number beetwen 1 and 10")
+if (number = 1){
+   console.log("You entered one" )
+}
+ if (number = 2 ){
+   console.log("You entered two")
+}
+ if (number = 3){
+   console.log ("you entered three")
+   }
+if else {console.log ("Number is not between1 and 3")}
