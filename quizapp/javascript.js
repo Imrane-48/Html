d3e88b911074a49a775733fc9d2f390966fe4cb5
Imrane Bouhaddou <Imranebhd8gmail.com>
2024-07-29 08:48:59 +0100

    quiz website

--- quizapp/javascript.js
+++ quizapp/javascript.js
@@ -1,42 +1,16 @@
-const question = [
-    {
-        question : "what is the name of luffy's fruit ",
-        answers: [
-            { text: "Mera Mera no Mi/Flame-Flame Fruit", correct: false},
-            { text: "Ito Ito no Mi/String-String Fruit", correct: false},
-            { text: "Gomu Gomu no Mi/Gum-Gum Fruit", correct: true},
-            { text: "Bara Bara no Mi/Chop-Chop Fruit", correct: false},
-            
-        ]
-    },
-    {
-        question : "what is the name of Gaara's Biju  ",
-        answers: [
-            { text: "Shukaku", correct:true},
-            { text: "Isobu", correct: false},
-            { text: "Kurama", correct: false},
-            { text: "Saiken", correct: false},
-            
-        ]
-    },
-    {
-        question : "what is the first breathing style in demon slayer",
-        answers: [
-            { text: "WInd breathing", correct: false},
-            { text: "Flame breathing", correct: false},
-            { text: "Water breathing", correct: false},
-            { text: "Sun breathing", correct: true},
-            
-        ]
-    },
-    {
-        question : " what is the name of Deku's power",
-        answers: [
-            { text: "All for one", correct: false},
-            { text: "One for one", correct: false},
-            { text: " One for all ", correct: true},
-            { text: "All for all", correct: false},
-            
-        ]
-    },
-]
+function result(){
+    var Mark=0;
+    if( document.getElementById("Correct1").checked) {
+        Mark++;
+    }
+    if( document.getElementById("Correct2").checked) {
+        Mark++;
+    }
+    if( document.getElementById("Correct3").checked) {
+        Mark++;
+    }
+    if( document.getElementById("Correct4").checked) {
+        Mark++;
+    }
+    document.write("youre score is:" +Mark)
+}
